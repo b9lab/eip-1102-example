26e43053b94570da7117dea3b3b25865e5eb6071
Xavier Lepretre <xavier.lepretre@gmail.com>
2019-09-04 21:41:26 +0100

    Simple project with injected web3.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Simple Ethereum Dapp
+
+It showcases how to access an injected Ethereum provider, in this case `window.web3`, as it was pre-EIP-1102, a.k.a. from the start.
+
+The GUI displays:
+
+* whether you are connected to Ethereum
+* the balances of your accounts, if you have any
+* the balance of any address you paste in the input box
+
+After cloning it, to run this program, you need to:
+
+```sh
+$ npm install
+$ npm run dev
+```
+Then open [http://127.0.0.1:8080](http://127.0.0.1:8080) in your preferred browser.
---
+++ app/app.js
@@ -0,0 +1,51 @@
+const Web3 = require("web3");
+const $ = require("jquery");
+
+window.addEventListener('load', async function() {
+
+    if (typeof web3 !== 'undefined') {
+        // Supports injected Ethereum providers.
+        window.web3 = new Web3(web3.currentProvider);
+    } else {
+        // Your preferred fallback.
+        window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545')); 
+    }
+
+    try {
+        $("#connected").html(`You are connected to network ${await window.web3.eth.net.getId()}`);
+    } catch(error) {
+        $("#connected").html(`You are have no access to Ethereum: ${error}`);
+    }
+
+    $("#otherAddress").change(async function() {
+        try {
+            $("#otherBalance").html(await window.web3.eth.getBalance($(this).val()));
+        } catch(error) {
+            $("#otherBalance").html(error);
+        }
+    });
+
+    const displayMyAccounts = accounts => {
+        try {
+            if (accounts.length == 0) {
+                $("#myAddresses").html("<tr><td colspan='2'>You have no addresses</td></tr>");
+            } else {
+                $("#myAddresses").html("");
+                accounts.forEach(async myAddress => $("#myAddresses").append(`<tr>
+                        <td>${myAddress}</td>
+                        <td>${await window.web3.eth.getBalance(myAddress)}</td>
+                    </tr>`)
+                );
+            }
+        } catch(error) {
+            $("#myAddresses").html(error);
+        }
+    };
+
+    try {
+        displayMyAccounts(await window.web3.eth.getAccounts());
+    } catch(error) {
+        $("#myAddresses").html(`Failed to get your addresses: ${error}`);
+    }
+
+});
